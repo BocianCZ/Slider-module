e981c42fddcb4d4e8c608678a2f82878161ff2aa
arturmamedov <arturmamedov1993@gmail.com>
2017-05-19 22:23:49 +0200

    Slider - Slide upload image on create with SlideWasCreated Event in EloquentSlideRepository create()

--- Repositories/Eloquent/EloquentSlideRepository.php
+++ Repositories/Eloquent/EloquentSlideRepository.php
@@ -2,21 +2,24 @@
 
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 use Modules\Slider\Repositories\SlideRepository;
+use Modules\Slider\Events\SlideWasCreated;
 
 class EloquentSlideRepository extends EloquentBaseRepository implements SlideRepository
 {
+
+    /**
+     * Override for add the event on create and link media file
+     *
+     * @param mixed $data Data from POST request form
+     *
+     * @return object The created entity
+     */
     public function create($data)
     {
-        $slide = $this->model->create($data);
+        $slide = parent::create($data);
+
+        event(new SlideWasCreated($slide, $data));
 
         return $slide;
     }
-
-    public function update($menuItem, $data)
-    {
-        $menuItem->update($data);
-
-        return $menuItem;
-    }
-
 }
