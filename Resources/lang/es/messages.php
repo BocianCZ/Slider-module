0ae3a46229bb2604785fd69577c7ccf56ded87c5
Luis Mas <lmas@it2b.es>
2017-06-05 09:54:57 +0200

    Add Spanish translations
    Spanish Translations

---
+++ Resources/lang/es/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /* Slider management */
+    'slider created' => 'Slider creado con éxito.',
+    'slider not found' => 'Slider no encontrado.',
+    'slider updated' => 'Slider actualizado con éxitoe.',
+    'slider deleted' => 'Slider borrado con éxito.',
+    /* SliderItem management */
+    'slide created' => 'Slide successfully created.',
+    'slide not found' => 'Slide no encontrado.',
+    'slide updated' => 'Slide actualizado con éxito.',
+    'slide deleted' => 'Slide borrado con éxito.',
+];
