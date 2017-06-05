68b47c92da0a79f12927165934675c136d8fc17b
Luis Mas <lmas@it2b.es>
2017-06-05 10:00:38 +0200

    Spanish translations. Fix typo and missing translation

--- Resources/lang/es/messages.php
+++ Resources/lang/es/messages.php
@@ -4,10 +4,10 @@ return [
     /* Slider management */
     'slider created' => 'Slider creado con éxito.',
     'slider not found' => 'Slider no encontrado.',
-    'slider updated' => 'Slider actualizado con éxitoe.',
+    'slider updated' => 'Slider actualizado con éxito.',
     'slider deleted' => 'Slider borrado con éxito.',
     /* SliderItem management */
-    'slide created' => 'Slide successfully created.',
+    'slide created' => 'Slide creado con éxito.',
     'slide not found' => 'Slide no encontrado.',
     'slide updated' => 'Slide actualizado con éxito.',
     'slide deleted' => 'Slide borrado con éxito.',
