27e1e011309137e07f044e741e26e668218c78d5
David Cap <david.cap@bocians.net>
2019-02-12 11:08:38 -0800

    add migration and Slide entity properties for Youtube video

--- Entities/Slide.php
+++ Entities/Slide.php
@@ -4,11 +4,28 @@ namespace Modules\Slider\Entities;
 
 use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Collection;
+use Modules\Media\Entities\File;
 use Modules\Media\Support\Traits\MediaRelation;
 use Modules\Page\Entities\Page;
 
+/**
+ * Class Slide
+ * @package Modules\Slider\Entities
+ * @property int $slider_id
+ * @property int $page_id
+ * @property int $position
+ * @property string $target
+ * @property string $title
+ * @property string $caption
+ * @property string $uri
+ * @property string $url
+ * @property bool $active
+ * @property string $external_image_url
+ * @property string $youtube_video_url
+ * @property string $custom_html
+ * @property File[]|Collection $files
+ */
 class Slide extends Model
 {
     use Translatable, MediaRelation;
@@ -33,8 +50,10 @@ class Slide extends Model
         'url',
         'active',
         'external_image_url',
+        'youtube_video_url',
         'custom_html',
     ];
+
     protected $table = 'slider__slides';
 
     /**
@@ -54,7 +73,6 @@ class Slide extends Model
 
     /**
      * Check if page_id is empty and returning null instead empty string
-     * @return number
      */
     public function setPageIdAttribute($value)
     {
@@ -104,4 +122,24 @@ class Slide extends Model
 
         return $this->linkUrl;
     }
+
+    /**
+     * @return bool
+     */
+    public function isYoutubeVideo()
+    {
+        return !empty($this->youtube_video_url);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getYoutubeVideoId()
+    {
+        preg_match("/https:\/\/www\.youtube\.com\/watch\?v=([a-z0-9].*)/i", $this->external_image_url, $matches);
+
+        return isset($matches[1]) ? $matches[1] : null;
+    }
+
+
 }
