b860846c1bda45b6e8647ee3add8793e393c432f
David Cap <david.cap@bocians.net>
2019-10-19 20:53:29 -0700

    fix slider name unique validation bug (and validation messages) closes #68

--- Resources/lang/cs/validation.php
+++ Resources/lang/cs/validation.php
@@ -2,5 +2,7 @@
 
 return [
     'name is required' => 'Jméno je vyžadováno',
+    'name must be unique' => 'Jméno musí být unikátní',
     'system name is required' => 'Systémové jméno je vyžadováno',
+    'system name must be unique' => 'Systémové jméno musí být unikátní',
 ];
